08e1c2ae5c028caa323ef24f5d35ffd6b19047f5
sahithisaranya <sahithisaranya@gmail.com>
2023-02-04 18:00:44 +0530

    login validation complete

--- loginScript.js
+++ loginScript.js
@@ -13,12 +13,17 @@ if (loginBtnElement) {
         const userNameLocal = localStorage.getItem('username');
         const pswdLocal = localStorage.getItem('password');
 
+        const displayElement=document.getElementById('msg');
+
         if (userName === userNameLocal && pswd === pswdLocal) {
+            displayElement.innerHTML="Redirecting...";
             console.log("login successful");
         }
         else {
+            displayElement.innerHTML="Invalid username/password";
             console.log("Entered details are incorrect! Try again!!");
         }
 
+        event.preventDefault();
     });
 }
