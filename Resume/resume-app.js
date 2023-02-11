dfa59e5478d92980d686efa3253e7d20ff5e877e
sahithisaranya <sahithisaranya@gmail.com>
2023-02-11 20:24:28 +0530

    previous and next buttons

--- Resume/resume-app.js
+++ Resume/resume-app.js
@@ -1,115 +1,190 @@
 import { Resume } from "./resume-api.js";
 
-class ResumeApp{
-    constructor(){
+class ResumeApp {
+
+
+    constructor() {
         console.log("resumeapp constructor called");
-        
+        this.id = 0;
+        let json;
+        this.json = json;
     }
-    init(){
-        // const bodyElement=document.querySelector(".resume-body");
-        // bodyElement.style.backgroundColor="red";
-        window.history.forward();
-        this.addEventHandlers();
+    init() {
+        this.id = 0;
+        this.loadJsonFile();
     }
 
-//     const previousBtnElement = document.getElementById('previous');
-// if (previousBtnElement) {
-//     previousBtnElement.addEventListener("click", ()=> {
-//         resumeData=getJSONData();
-//         console.log(resumeData);
-        
-//     });
-// }
-    addEventHandlers(){
+    loadJsonFile() {
+
+        const resumeObj = new Resume();
+        resumeObj.getJSONData().then((response) => {
+            this.json = response;
+            if (response.resume.length > 0) {
+                document.getElementById("previous").disabled = true;
+                document.getElementById("next").disabled = false;
+            }
+            this.updateControls(this.json);
+            this.addEventListeners();
+        });
+    }
+
+    addEventListeners() {
         console.log("addeventhandlers called");
-        const resume=new Resume();
-        resume.getJSONData().then((response)=>{
-            console.log(response);
-            this.updateControls(response);
-        })
-       
+        console.log(this.json);
+        const prevBtnElement = document.getElementById("previous");
+        const nextBtnElement = document.getElementById("next");
+        let currentObj = this;
+
+        prevBtnElement.onclick = function () {
+            console.log("previous button clicked");
+            currentObj.previous();
+        }
+
+
+        nextBtnElement.onclick = function () {
+            console.log("next button clicked");
+            console.log(currentObj.json);
+            currentObj.next();
+        }
+
     }
 
-    updateControls(jsonData){
-        const nameElement=document.querySelector(".applicant-name");
-        nameElement.innerHTML=`${jsonData.resume[0].basics.name}`;
+    previous() {
+        this.id--;
+        this.displayPreviousResume();
+    }
 
-        const appliedForElement=document.querySelector(".applied-for");
-        appliedForElement.innerHTML=`${jsonData.resume[0].basics.AppliedFor}`;
+    next() {
+        this.id++;
+        this.displayNextResume();
+    }
+
+    displayNextResume() {
+        if (this.id >= this.json.resume.length-1) {
+            document.getElementById("next").disabled = true;
+        }
+        else if (this.id > 0 && this.id < this.json.resume.length) {
+            document.getElementById("previous").disabled = false;
+        }
+        this.updateControls(this.json);
+    }
+
+    displayPreviousResume() {
+
+        if (this.id <= 0) {
+            document.getElementById("previous").disabled = true;
+        }
+        else if (this.id >= 0 && this.id < this.json.resume.length) {
+            document.getElementById("next").disabled = false;
+        }
+        this.updateControls(this.json);
+    }
+    // const nextBtnElement = document.getElementById("next");
+    // const previousBtnElement = document.getElementById("previous");
+
+    // if (previousBtnElement) {
+    //     previousBtnElement.addEventListener("click", () => {
+    //         console.log("previous button clicked");
+    //         this.id--;
+    //         
+    //         this.updateControls(response);
+    //     });
+    // }
+    // else if (nextBtnElement) {
+    //     nextBtnElement.addEventListener("click", () => {
+    //         console.log("next button clicked");
+    //         this.id++;
+    //         if (this.id >= response.resume.length) {
+    //             document.getElementById("next").disabled = true;
+    //         }
+    //         else if (this.id > 0 && this.id < response.resume.length) {
+    //             document.getElementById("previous").disabled = false;
+    //         }
+    //         this.updateControls(response);
+    //     });
+
+    // }
+
+    updateControls(jsonData) {
+        const nameElement = document.querySelector(".applicant-name");
+        nameElement.innerHTML = `${jsonData.resume[this.id].basics.name}`;
+
+        const appliedForElement = document.querySelector(".applied-for");
+        appliedForElement.innerHTML = `${jsonData.resume[this.id].basics.AppliedFor}`;
 
         // const applicantImg=document.querySelector(".applicant-img");
-        // if(`${jsonData.resume[0].basics.image}`===""){
-        //     applicantImg.src=`${jsonData.resume[0].basics.image}`;
+        // if(`${jsonData.resume[this.id].basics.image}`===""){
+        //     applicantImg.src=`${jsonData.resume[this.id].basics.image}`;
         // }
         // else{
         //     applicantImg.src="./applicant.png"
         // }   
-            
 
-        const personalInfoElement=document.querySelector(".personal-info");
-        personalInfoElement.innerHTML=`
-        <p style="color:black;text-align:right;font-size: 12px;font-weight: bolder;">${jsonData.resume[0].basics.phone}</p>
-        <p style="color:black;text-align:right;font-size: 12px;font-weight: bolder;">${jsonData.resume[0].basics.email}</p>
+
+        const personalInfoElement = document.querySelector(".personal-info");
+        personalInfoElement.innerHTML = `
+        <p style="color:black;text-align:right;font-size: 12px;font-weight: bolder;">${jsonData.resume[this.id].basics.phone}</p>
+        <p style="color:black;text-align:right;font-size: 12px;font-weight: bolder;">${jsonData.resume[this.id].basics.email}</p>
         <a style="color:red;float:right;font-size: 12px;font-weight: bolder;" 
-        href="${jsonData.resume[0].basics.profiles.url}">${jsonData.resume[0].basics.profiles.network}</a><br>`;
+        href="${jsonData.resume[this.id].basics.profiles.url}">${jsonData.resume[this.id].basics.profiles.network}</a><br>`;
 
-        const techSkillsElement=document.querySelector(".technical-skills");
-        let skillsText="";
-        for(let i=0;i<jsonData.resume[0].skills.keywords.length;i++){
-            skillsText+=jsonData.resume[0].skills.keywords[i]+"<br>";
+        const techSkillsElement = document.querySelector(".technical-skills");
+        let skillsText = "";
+        for (let i = 0; i < jsonData.resume[this.id].skills.keywords.length; i++) {
+            skillsText += jsonData.resume[this.id].skills.keywords[i] + "<br>";
         }
-        techSkillsElement.innerHTML=`
+        techSkillsElement.innerHTML = `
         <p style="color:black;text-align:right;font-size: 12px;font-weight: bolder;">${skillsText}</p>
         `;
-       
-        const hobbiesElement=document.querySelector(".hobbies");
-        let hobbiesText="";
-        for(let i=0;i<jsonData.resume[0].interests.hobbies.length;i++){
-            hobbiesText+=jsonData.resume[0].interests.hobbies[i]+"<br>";
+
+        const hobbiesElement = document.querySelector(".hobbies");
+        let hobbiesText = "";
+        for (let i = 0; i < jsonData.resume[this.id].interests.hobbies.length; i++) {
+            hobbiesText += jsonData.resume[this.id].interests.hobbies[i] + "<br>";
         }
-        hobbiesElement.innerHTML=`
+        hobbiesElement.innerHTML = `
         <p style="color:black;text-align:right;font-size: 12px;font-weight: bolder;">${hobbiesText}</p>
         `
 
-        const workexpElement=document.querySelector(".exp-prev-company");
-        workexpElement.innerHTML=`
-            <p><b>Company Name: </b>${jsonData.resume[0].work["Company Name"]}</p>
-            <p><b>Position: </b>${jsonData.resume[0].work["Position"]}</p>
-            <p><b>Start Date: </b>${jsonData.resume[0].work["Start Date"]}</p>
-            <p><b>End Date: </b>${jsonData.resume[0].work["End Date"]}</p>
-            <p><b>Summary: </b>${jsonData.resume[0].work["Summary"]}</p>`
+        const workexpElement = document.querySelector(".exp-prev-company");
+        workexpElement.innerHTML = `
+            <p><b>Company Name: </b>${jsonData.resume[this.id].work["Company Name"]}</p>
+            <p><b>Position: </b>${jsonData.resume[this.id].work["Position"]}</p>
+            <p><b>Start Date: </b>${jsonData.resume[this.id].work["Start Date"]}</p>
+            <p><b>End Date: </b>${jsonData.resume[this.id].work["End Date"]}</p>
+            <p><b>Summary: </b>${jsonData.resume[this.id].work["Summary"]}</p>`
 
-        const projectsElement=document.querySelector(".projects");
-        projectsElement.innerHTML=`
-        <p><b>${jsonData.resume[0].projects["name"]}</b>${jsonData.resume[0].projects["description"]}</p>`
+        const projectsElement = document.querySelector(".projects");
+        projectsElement.innerHTML = `
+        <p><b>${jsonData.resume[this.id].projects["name"]}</b>${jsonData.resume[this.id].projects["description"]}</p>`
 
-        const educationElement=document.querySelector(".education");
-        educationElement.innerHTML=`
+        const educationElement = document.querySelector(".education");
+        educationElement.innerHTML = `
         <ul>
-            <li><b>UG: </b> ${jsonData.resume[0].education["UG"]["institution"]}, ${jsonData.resume[0].education["UG"]["course"]}, ${jsonData.resume[0].education["UG"]["Start Date"]}, ${jsonData.resume[0].education["UG"]["End Date"]}, ${jsonData.resume[0].education["UG"]["cgpa"]}</li>
-            <li><b>PU: </b> ${jsonData.resume[0].education["Senior Secondary"]["institute"]}, ${jsonData.resume[0].education["Senior Secondary"]["cgpa"]}</li>
-            <li><b>High School: </b> ${jsonData.resume[0].education["High School"]["institute"]}, ${jsonData.resume[0].education["High School"]["cgpa"]}</li>
+            <li><b>UG: </b> ${jsonData.resume[this.id].education["UG"]["institution"]}, ${jsonData.resume[this.id].education["UG"]["course"]}, ${jsonData.resume[this.id].education["UG"]["Start Date"]}, ${jsonData.resume[this.id].education["UG"]["End Date"]}, ${jsonData.resume[this.id].education["UG"]["cgpa"]}</li>
+            <li><b>PU: </b> ${jsonData.resume[this.id].education["Senior Secondary"]["institute"]}, ${jsonData.resume[this.id].education["Senior Secondary"]["cgpa"]}</li>
+            <li><b>High School: </b> ${jsonData.resume[this.id].education["High School"]["institute"]}, ${jsonData.resume[this.id].education["High School"]["cgpa"]}</li>
         </ul>`
 
-        const internshipElement=document.querySelector(".internship");
-        internshipElement.innerHTML=`
+        const internshipElement = document.querySelector(".internship");
+        internshipElement.innerHTML = `
         <ul>
-            <li><b>Company Name: </b> ${jsonData.resume[0].Internship["Company Name"]}</li>
-            <li><b>Position: </b> ${jsonData.resume[0].Internship["Position"]}</li>
-            <li><b>Start Date: </b> ${jsonData.resume[0].Internship["Start Date"]}</li>
-            <li><b>End Date: </b> ${jsonData.resume[0].Internship["End Date"]}</li>
-            <li><b>Summary: </b> ${jsonData.resume[0].Internship["Summary"]}</li>
+            <li><b>Company Name: </b> ${jsonData.resume[this.id].Internship["Company Name"]}</li>
+            <li><b>Position: </b> ${jsonData.resume[this.id].Internship["Position"]}</li>
+            <li><b>Start Date: </b> ${jsonData.resume[this.id].Internship["Start Date"]}</li>
+            <li><b>End Date: </b> ${jsonData.resume[this.id].Internship["End Date"]}</li>
+            <li><b>Summary: </b> ${jsonData.resume[this.id].Internship["Summary"]}</li>
         </ul>`
 
-        const achievementsElement=document.querySelector(".achievements");
-        let achievementsText="<ul>";
-        for(let i=0;i<jsonData.resume[0].achievements["Summary"].length;i++){
-            achievementsText+=`<li>${jsonData.resume[0].achievements["Summary"][i]}</li>`;
+        const achievementsElement = document.querySelector(".achievements");
+        let achievementsText = "<ul>";
+        for (let i = 0; i < jsonData.resume[this.id].achievements["Summary"].length; i++) {
+            achievementsText += `<li>${jsonData.resume[this.id].achievements["Summary"][i]}</li>`;
         }
-        achievementsText+="</ul>";
-        achievementsElement.innerHTML=achievementsText;
+        achievementsText += "</ul>";
+        achievementsElement.innerHTML = achievementsText;
 
     }
 }
 
-export {ResumeApp}
+export { ResumeApp }
